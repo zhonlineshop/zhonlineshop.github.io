9baf998004ea42cad60621f51456aa062c19438d
dangbingqu <ss88817@163.com>
2017-11-07 00:52:32 +0800

    20171107

--- assets/js/data.js
+++ assets/js/data.js
@@ -19,7 +19,14 @@ var dataMap = {
 				desc: "background3"
 			}
 		],
-		productInfo: "scooter介绍"
+		productInfo: "<ul><li>Price: 799.99 euros</li>"+
+			"<li>Time to market: Summer in 2014</li>"+
+			"<li>Origin: China</li>"+
+			"<li>Brand: East Sunshine</li>"+
+			"<li>The highest speed: 20km/h</li>"+
+			"<li>The number of loads: 2 adults</li>"+
+			"<li>Color: red/green/white</li>"+
+			"</ul>"
 	},
 	"shoe":{
 		imageFolder:"assets/images/shoe/",
@@ -41,7 +48,17 @@ var dataMap = {
 				desc: "background3"
 			}
 		],
-		productInfo: "shoe介绍"
+		productInfo: "<ul><li>Price: 27 euros</li>"+
+			"<li>Time to market: Summer in 2014</li>"+
+			"<li>Color: pink/blue/white</li>"+
+			"<li>Texture: Leather</li>"+
+			"<li>Imported</li>"+
+			"<li>Synthetic sole</li>"+
+			"<li>Leather shoe with hook-and-loop strap, padded collar, and comfortable seamless lining</li>"+
+			"<li>Designed for young children who are learning to stand</li>"+
+			"<li>Sensory pods at heel and forefoot for enhanced ground feel </li>"+
+			"<li>Wipe clean with damp cloth</li>"+
+			"</ul>"
 	},
 	"chair":{
 		imageFolder:"assets/images/chair/",
@@ -63,7 +80,18 @@ var dataMap = {
 				desc: "background3"
 			}
 		],
-		productInfo: "chair介绍"
+		productInfo: "<ul><li>Price: 43 euros</li>"+
+			"<li>Brand: Vinsani</li>"+
+			"<li>Adjustable height</li>"+
+			"<li>Texture: Leather</li>"+
+			"<li>Weight: 7kg </li>"+
+			"<li>Shipping Weight: 10kg</li>"+
+			"<li>Color: red/green/white</li>"+
+			"<li>Origin: China</li>"+
+			"<li>Texture: steel plating</li>"+
+			"<li>360 degrees swivel function</li>"+
+			"<li>Product Dimensions: 37*37*108 cm</li>"+
+			"</ul>"
 	},
 	"car":{
 		imageFolder:"assets/images/car/",
@@ -85,6 +113,15 @@ var dataMap = {
 				desc: "background3"
 			}
 		],
-		productInfo: "car介绍"
+		productInfo: "<ul><li>Price: 30,000 euros</li>"+
+			"<li>Manufacturer: Subaru Corporation</li>"+
+			"<li>Production: 2016</li>"+
+			"<li>Standard seat: 4</li>"+
+			"<li>The highest speed: 240</li>"+
+			"<li>Color: blue/ white</li>"+
+			"<li>Car weight: 1800 kg</li>"+
+			"<li>Texture of the seat: Leather</li>"+
+			"<li>The volume of oil tank: 80L</li>"+
+			"</ul>"
 	}
 }
